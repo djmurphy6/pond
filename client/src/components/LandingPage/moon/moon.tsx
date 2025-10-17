729f2d5df62968824aeca12ae330cd42edcd56f3
Owen W <owilson@uoregon.edu>
2025-10-17 15:20:54 -0700

    moon changes

--- client/src/components/LandingPage/moon/moon.tsx
+++ client/src/components/LandingPage/moon/moon.tsx
@@ -6,10 +6,11 @@ import "./moon.css";
 export default function Duck() {
     return (
         <div className="moon">
-            <div className="hole1"></div>
-            <div className="hole2"></div>
-            <div className="hole3"></div>
-            <div className="hole4"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
         </div>
     )
 }
