44bbcd83c126d9fe6d62ca124e1b2d706dbb6745
owenWilson-UO <owilson@uoregon.edu>
2025-10-17 16:28:28 -0700

    moon update

--- client/src/components/LandingPage/moon/moon.tsx
+++ client/src/components/LandingPage/moon/moon.tsx
@@ -11,6 +11,9 @@ export default function Duck() {
             <div className="hole"></div>
             <div className="hole"></div>
             <div className="hole"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
+            <div className="hole"></div>
         </div>
     )
 }
