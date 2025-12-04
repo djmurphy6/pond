0de5e588879b2be9f0052ac0797ebd13efa22827
Dom Ellis <aprules68@yahoo.com>
2025-12-04 13:25:45 -0800

    added previous middleware as a comment

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -13,3 +13,40 @@ export function middleware(request: NextRequest) {
 export const config = {
     matcher: ["/((?!api|_next|static|favicon.ico).*)"],
 };
+
+// export function middleware(request: NextRequest) {
+//     const token = request.cookies.get("refreshToken")?.value;
+
+//     // List of routes that require authentication
+//     const protectedRoutes = ["/dashboard", "/dashboard/you/listings"];
+
+//     const isProtected = protectedRoutes.some((path) =>
+//         request.nextUrl.pathname.startsWith(path)
+//     );
+
+//     const fromRegister = (request.nextUrl.searchParams.get("from") === "register")
+//     const unAuthorized = (request.nextUrl.searchParams.get("error") === "unauthorized");
+
+//     const isLogin = request.nextUrl.pathname.startsWith("/login") && !unAuthorized && !fromRegister;
+
+//     if (isProtected && !token) {
+//         const loginUrl = new URL("/login", request.url);
+
+//         if (unAuthorized) {
+//             loginUrl.searchParams.set("error", "unauthorized");
+//         }
+//         if (fromRegister) {
+//             loginUrl.searchParams.set("from", "register");
+//         }
+
+//         return NextResponse.redirect(loginUrl);
+//     } else if (isLogin && token) {
+//         return NextResponse.redirect(new URL("/dashboard", request.url));
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/((?!api|_next|static|favicon.ico).*)"],
+// };
